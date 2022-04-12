6271bd1ce284dbd169052dff0351f605a415c248
MartinJepsen <mjeps17@student.aau.dk>
2022-04-12 18:29:36 +0200

    formulate as chain system

---
+++ setup/chain_system.m
@@ -0,0 +1,62 @@
+
+
+k = [50, 40, 30, 20, 30, 60];
+m = [0.48, 0.27, 0.0700, 0.0700, 0.090, 0.150];
+free_dof = numel(m);
+
+Kg = zeros(free_dof+1);
+Cg = Kg;
+Mg = diag(m);
+Kg_d = Kg;
+
+for i =1:free_dof
+   kl = [1 -1; -1 1]*k(i);
+   Kg(i:i+1,i:i+1) = Kg(i:i+1,i:i+1) + kl;
+
+   if i == dam(1)
+       kl = dam(2)*kl;
+   end
+   Kg_d(i:i+1,i:i+1) = Kg_d(i:i+1,i:i+1) + kl;
+end
+
+Kg(1,:)=[]; Kg(:,1)=[];
+Kg_d(1,:)=[]; Kg_d(:,1)=[];
+
+[Phi, Lambda] = eig(Kg, Mg);
+C_tilde = diag([0.01, 0.015, 0.020, 0.025, 0.030, 0.035]) .* (2 * sqrt(diag(Lambda)));
+Cg = inv(Phi)' * C_tilde * inv(Phi);
+
+
+FE = FiniteElementModel();
+FE.Kg = Kg;
+FE.Kg_d = Kg_d;
+FE.Cg = Cg;
+FE.Mg = Mg;
+FE.n_dof = free_dof + 1;
+FE.mesh.topology = [1, 2;
+                    2, 3;
+                    3, 4;
+                    4, 5;
+                    5, 6;
+                    6, 7];
+FE.mesh.coordinates = [0, 0;
+                       1, 0;
+                       2, 0;
+                       3, 0;
+                       4, 0;
+                       5, 0;
+                       6, 0];
+n_el = size(FE.mesh.topology, 1);
+FE.mesh.n_el = n_el;
+FE.mesh.n_node = size(FE.mesh.coordinates, 1);
+FE.mesh.element_properties.L = ones(n_el,1);
+FE.mesh.element_properties.rot = [ones(n_el, 1), zeros(n_el, 1)];
+
+B = zeros(free_dof, FE.n_dof);
+B(1:size(B)+1:numel(B)) = -1;
+B = B - circshift(B,1,2);
+FE.B = B;
+FE.mesh.bc_dof = 1;
+
+clearvars -except FE free_dof
+
--- setup/set_up.m
+++ setup/set_up.m
@@ -1,6 +1,10 @@
 rng(1)
+
+dam = [1, 0.85];
+
+chain_system
+
 in_dof = [1, 3, 5];
-% in_dof = 1:12;
 out_dof = in_dof;
 dt = 0.02;                        % time increment size
 Nsamples = 50000;
@@ -9,15 +13,8 @@ u = randn(numel(in_dof), numel(t));
 blockrows = 40;
 
 nsr = 0.05;
-dam = [1, 0.85];
 
-FE = FiniteElementModel();
-FE.from_xlsx('structures/paper_truss.xlsx')
-FE.assembly('bar', dam);
-FE.apply_bc([1, 2, 9, 10]);
-FE.modal_damping(0.02);
-FE.strains_from_disp([])
-cdis = zeros(numel(out_dof), 12);
+cdis = zeros(numel(out_dof), free_dof);
 for ii=1:numel(out_dof);
     cdis(ii, out_dof(ii)) = 1;
 end
@@ -28,7 +25,7 @@ Cg = FE.Cg;
 Mg = FE.Mg;
 
 SS_exact = StateSpaceModel();
-SS_exact.set_io(1:12, 1:12, 24);
+SS_exact.set_io(1:free_dof, 1:free_dof, 2*free_dof);
 SS_exact.dt_from_FE(Kg, Cg, Mg, dt);
 SS_exact.get_modal_parameters();
 % SS_exact.time_response(u, t, 0, true);
@@ -39,11 +36,9 @@ SS_exact.transfer_matrix(s);
 
 % Exact, damaged
 SS_exact_d = StateSpaceModel();
-SS_exact_d.set_io(1:12, 1:12, 24);
+SS_exact_d.set_io(1:free_dof, 1:free_dof, 2*free_dof);
 SS_exact_d.dt_from_FE(FE.Kg, FE.Cg, FE.Mg, dt);
 SS_exact_d.to_ct();
 SS_exact_d.transfer_matrix(s);
 
 export_gain_pars
-% i = i+1;
-% end
