9525020e5ceea90334bfa88cf46ccc49e71cb038
MartinJepsen <mjeps17@student.aau.dk>
2022-04-16 14:00:03 +0200

    add more results and gains #13

--- setup/run_all.m
+++ setup/run_all.m
@@ -1,12 +1,12 @@
 clear
-damages = [3, 5, 7, 9, 12]';
+damages = [1, 2, 12, 13, 14]';
 rng(1)
 damages(:, 2) = 0.95;
 
 
 for foo = 1:size(damages, 1)
     dam = damages(foo, :);
-%     estimate_models
-gaindesign_2
-gaindesign_3
+    estimate_models
+    gaindesign_2
+    gaindesign_3
 end
