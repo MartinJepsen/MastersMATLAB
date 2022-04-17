5eb1a557c7e91f6bc7697219d9f691758c1c83ab
MartinJepsen <mjeps17@student.aau.dk>
2022-04-17 09:14:50 +0200

    add results for 90% damage

--- setup/run_all.m
+++ setup/run_all.m
@@ -1,12 +1,10 @@
 clear
-damages = [1, 2, 12, 13, 14]';
+damages = [3, 5, 7, 9]';
 rng(1)
-damages(:, 2) = 0.95;
+damages(:, 2) = 0.90;
 
 
 for foo = 1:size(damages, 1)
     dam = damages(foo, :);
     estimate_models
-    gaindesign_2
-    gaindesign_3
 end
