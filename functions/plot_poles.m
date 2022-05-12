8e19d5142c6ae25fb8f07d1ce54658f7855a3062
Martin Jepsen <MAJP@ramboll.dk>
2022-05-12 16:32:20 +0200

    simplify plot_poles

--- functions/plot_poles.m
+++ functions/plot_poles.m
@@ -1,5 +1,5 @@
 
-function plot_poles(Lambda_ref, Lambda_ex)
+function f = plot_poles(Lambda_ref, Lambda_ex, s)
 % PLOT_POLES(Lambda_ref, Lambda_ex)
 % plot poles of two systems in the complex plane.
 % Arguments:
@@ -7,47 +7,19 @@ function plot_poles(Lambda_ref, Lambda_ex)
 % Lambda_ex: column vector containing poles  (sorted by eigenfrequency)
 
 f = figure;
-f.Position([3, 4]) = [8.5, 5];
+f.Units = 'centimeters';
+f.Position([3, 4]) = [15, 8];
 hold on; grid on
+plot(real(Lambda_ref), imag(Lambda_ref), 'k.', 'MarkerSize', 15)
+plot(real(Lambda_ex), imag(Lambda_ex), 'r.', 'MarkerSize', 12)
 
-roots = [Lambda_ref];
-n = numel(roots);
-
-for i = 1:2:n   
-  q(i) = plot( [real(roots(i)) real(roots(i+1))],[imag(roots(i)) imag(roots(i+1))],...
-      'k.-','LineWidth',1,'MarkerSize',15,'LineStyle','none') ;
+try
+    plot(real(s), imag(s), 'bx', 'MarkerSize', 15);
+catch
 end
+   
 
-roots = [Lambda_ex];
-n = size(roots,1);
-
-%% Plots in pairs
-% for j = 1:size(roots,2)
-% for i = 1:2:n
-%     
-%     e(i) = plot( [real(roots(i,j)) real(roots(i+1,j))],[imag(roots(i,j)) imag(roots(i+1,j))],'r.','MarkerSize',15);
-%     string = strjoin({'\lambda_',num2str((i-1)/2+1),' snr_',num2str(j)},'');
-%     string = strjoin({'\lambda_{',num2str((i-1)/2+1)},'}');
-%     text(real(roots(i,j)),imag(roots(i,j)),string,'Interpreter','tex','FontSize',fs,'FontName','palatino linotype')
-%     
-% end
-% end
-
-%% Plots individually
-for j = 1:size(roots,2)
-    for i = 1:n       
-        e(i) = plot(real(roots(i,j)), imag(roots(i,j)),'r.','MarkerSize',5);
-        string = strjoin({'\lambda_',num2str(i)});
-    %     string = strjoin({'\lambda_{',num2str((i-1)/2+1)},'}');
-%         text(real(roots(i,j)), imag(roots(i,j)),string,'Interpreter','tex','FontSize',fs,'FontName','palatino linotype')
-        
-    end
-end
-
-% set(gca, 'XAxisLocation','origin')
-% set(gca, 'YAxisLocation','origin')
 set(gca, 'FontName','Palatino Linotype')
-
 a = gca;
 a.XLabel.Interpreter = 'Latex';
 a.XLabel.String = '$\Re(s)$';
@@ -58,14 +30,16 @@ a.XLabel.Color = 'k';
 
 a.YLabel.Interpreter = 'Latex';
 a.YLabel.String = '$\Im(s)$';
-% a.YLabel.HorizontalAlignment = 'center';
-% a.YLabel.VerticalAlignment = 'bottom';
-% a.YLabel.Position = [2, 0.95*a.YLim(2), 0];
 a.YLabel.Color = 'k';
+a.YLabel.HorizontalAlignment = 'right';
+a.YLabel.VerticalAlignment = 'middle';
 a.YLabel.Rotation = 0;
 
-xline(0)
-yline(0)
-%%
+xline(0, 'k')
+yline(0, 'k')
 
-legend([q(1), e(1)], {'Exact CL', 'Estimated CL'},'Location','south east')
+if exist('s', 'var')
+    legend('Exact OL', 'Exact CL', 's', 'Location','southeast')
+else
+    legend('Exact OL', 'Exact CL', 'Location','southeast')
+end
