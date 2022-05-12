7a38023a4ee9aa6c916ae11b956fbeff80e36bb2
Martin Jepsen <MAJP@ramboll.dk>
2022-05-12 11:02:40 +0200

    get damaged SS model from damaged system matrices
    - deprecate export_gain_pars.m

--- setup/set_up.m
+++ setup/set_up.m
@@ -13,6 +13,14 @@ FE.apply_bc([1, 2, 9, 10]);
 FE.modal_damping(0.02);
 FE.strains_from_disp([])
 
+free_dof = size(FE.Kg, 1);
+n_dof = FE.n_dof;
+m = numel(out_dof);
+r = numel(in_dof);
+n_el = size(FE.mesh.topology, 1);
+B_strain = FE.B;
+bc = FE.mesh.bc_dof;
+
 Kg = FE.Kg;
 Kg_d = FE.Kg_d;
 Cg = FE.Cg;
@@ -29,9 +37,14 @@ Lambda = SS_exact.modal_parameters.Lambda;
 % Exact, damaged
 SS_exact_d = StateSpaceModel();
 SS_exact_d.set_io(1:12, 1:12, 24);
-SS_exact_d.dt_from_FE(FE.Kg, FE.Cg, FE.Mg, dt);
+SS_exact_d.dt_from_FE(FE.Kg_d, FE.Cg_d, FE.Mg, dt);
 SS_exact_d.get_modal_parameters();
 SS_exact_d.to_ct();
 Lambda_d = SS_exact_d.modal_parameters.Lambda;
 
-export_gain_pars
+B2 = StateSpaceModel().set_io(in_dof, out_dof, 24);
+cdis = zeros(m, free_dof);
+for ii=1:m
+    cdis(ii, out_dof(ii)) = 1;
+end
+idx = setdiff(1:n_dof, bc);
