3f5494859e00a7ec5c7a6f96ec3fa530ea7d777c
MJ <mjeps17@student.aau.dk>
2022-05-21 14:26:47 +0200

    add results for 2% model error

--- setup/run_all.m
+++ setup/run_all.m
@@ -3,10 +3,10 @@ damages = [1:14]';
 rng(1)
 damages(:, 2) = 0.80;
 nsr = 0.05;
-err = 0.00;
+err = 0.02;
 sensor = "acc";
 
-n_runs = 100;
+n_runs = 50;
 %% Run estimation
 
 for foo = 1:size(damages, 1)
