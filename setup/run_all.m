688d7021689b2c0cd5209d849ad64f0613ce2b04
MJ <mjeps17@student.aau.dk>
2022-05-26 22:14:35 +0200

    add chain results

--- setup/run_all.m
+++ setup/run_all.m
@@ -1,16 +1,16 @@
 clear
 damages = [1:10]';
 rng(1)
-damages(:, 2) = 0.95;
-
-nsr = 0.025;
+damages(:, 2) = 0.6;
+nsr = 0.05;
 err = 0.00;
+
 sensor = "dis";
 in_dof = [1:10];
 out_dof = in_dof;
-dt = 0.01;                            % time increment size
+dt = 0.005;                            % time increment size
 n_samples = 5000;
-blockrows = 10;
+blockrows = 40;
 n_runs = 25;
 set_up
 
