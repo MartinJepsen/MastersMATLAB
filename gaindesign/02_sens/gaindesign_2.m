7f5f46f092d9e94a2be66421cafdaa2203b2b093
MJ <mjeps17@student.aau.dk>
2022-05-30 10:22:24 +0200

    update scheme 2

--- gaindesign/02_sens/gaindesign_2.m
+++ gaindesign/02_sens/gaindesign_2.m
@@ -1,42 +1,65 @@
-set_up;
+% load('gaindesign/02_sens/SetUp.mat')
+[DamagedModels] = generate_damaged_models(ReferenceModels.FE, ReferenceModels.FE_e, damage);
 
-load("gaindesign/gain_pars")          % load system matrices
-damel = dam(:, 1);
+for polenum = 1:2:15
+
+im_fac = 1.2;
+Lambda = ReferenceModels.Lambda;
+s = complex(real(Lambda(polenum)), im_fac*imag(Lambda(polenum)));
+
+GeneralParameters.s = s;
+
+
+
 
 % Apply normalisation of stiffness perturbation
+Kg = ReferenceModels.Kg;
+Kg_d = DamagedModels.Kg_d;
 DeltaKg = Kg_d - Kg;
 DeltaKg(DeltaKg ~= 0) = DeltaKg(DeltaKg ~= 0) ./ abs(DeltaKg(DeltaKg ~= 0));
 Kg_d = DeltaKg - Kg;
 DeltaKg = DeltaKg(out_dof, in_dof);
+DamagedModels.DeltaKg = DeltaKg;
+
+Mg = ReferenceModels.Mg;
+Cg = ReferenceModels.Cg;
+Kg = ReferenceModels.Kg;
+out_dof = GeneralParameters.out_dof;
+in_dof = GeneralParameters.in_dof;
+
+H = (Mg*s^2 + Cg*s + Kg)^-1;
+H = H(out_dof, in_dof);
+dH = -H * DeltaKg * H;
+ReferenceModels.dH = dH;
+ReferenceModels.H = H;
 
 %% Genetic algorithm
-run = 0;
-good = 0;
 tic
-for run = 0
-    seed = ceil(abs(randn * randn) * 10)
-    rng(seed);
-    np = r*m; % No. of parameters
-    
-    ObjectiveFunction = @main_gain_design;
-    options = optimoptions('ga', 'Generations', 5000,...
-                            'PopulationSize', 200,...
-                            'FunctionTolerance',1e20,...
-                            'MaxStallGenerations', 200,...
-                            'PlotFcn', @gaplotbestf);
-    
-    [res, fval] = ga(ObjectiveFunction, np*2, [], [], [], [], [], [], [], options);
-       
-    re = reshape(res(1:np), r, m);
-    im = reshape(res(np+1:end), r, m);
-    K = complex(re, im);
-    
-    results{run+1,1} = K;
-    results{run+1,2} = fval;
 
+for run = 0
+r = GeneralParameters.r;
+m = GeneralParameters.m;
+np = r*m;
+
+ObjectiveFunction = @main_gain_design;
+options = optimoptions('ga', 'Generations', 5000,...
+                        'PopulationSize', 200,...
+                        'FunctionTolerance',1e-5,...
+                        'CrossoverFraction',0.5,...
+                        'MaxStallGenerations', 200);%,...
+%                         'PlotFcn', @gaplotbestf);
+
+[res, fval] = ga(ObjectiveFunction, np*2, [], [], [], [], [], [], [], options);
+   
+re = reshape(res(1:np), r, m);
+im = reshape(res(np+1:end), r, m);
+K = complex(re, im);
+
+results{run+1,1} = K;
+results{run+1,2} = fval;
 end
 
-[fvals, ind] = sort([results{:,2}], 'ascend');
+[fvals, ind] = sort([results{:,2}], "ascend");
 toc
 
 %% Sort and save results
@@ -46,22 +69,27 @@ for i = 1:numel(fvals)
 end
 
 K = gains{1,1};
-save(sprintf("gaindesign/02_sens/constrained/gains_%02d", damel),"K", 'gains')
+save(sprintf("gaindesign/02_sens/gain%d_%d_%0.3f.mat", damage(1,1), polenum, im_fac),"K", "gains", "s")
 beep
+end
+
 
 function [J] = main_gain_design(X)
 
-    in_dof = evalin('base', 'in_dof');
-    out_dof = evalin('base', 'out_dof');
-    m = evalin('base', 'm');
-    r = evalin('base', 'r');
-    B2 = evalin('base', 'B2');
-    cdis = evalin('base','cdis');
-    DeltaKg = evalin('base','DeltaKg');
-    s = evalin('base', 's');
-    Kg = evalin('base', 'Kg');
-    Cg = evalin('base', 'Cg');
-    Mg = evalin('base', 'Mg');
+    GeneralParameters = evalin('base', 'GeneralParameters');
+    ReferenceModels = evalin('base', 'ReferenceModels');
+    DeltaKg = evalin('base', 'DeltaKg');
+    r = GeneralParameters.r;
+    m = GeneralParameters.m;
+    s = GeneralParameters.s;
+    in_dof = GeneralParameters.in_dof;
+    out_dof = GeneralParameters.out_dof;
+    B2 = GeneralParameters.B2;
+    cdis = GeneralParameters.cdis;
+    Mg = ReferenceModels.Mg;
+    Cg = ReferenceModels.Cg;
+    Kg = ReferenceModels.Kg;
+
     
     % rearrange function argument to complex-valued gain matrix
     np = r*m;
@@ -70,9 +98,8 @@ function [J] = main_gain_design(X)
     K = complex(re, im);
     
     % OL
-    H = (Mg*s^2 + Cg*s + Kg)^-1;
-    H = H(out_dof, in_dof);
-    dH = -H * DeltaKg * H;
+    dH = ReferenceModels.dH;
+    H = ReferenceModels.H;
 
     % CL
     H_CL = (Mg*s^2 + Cg*s + Kg + B2*K*cdis)^-1;
