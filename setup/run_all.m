3e512a29860901446dd0c169e6f7d4416123cd1a
MJ <mjeps17@student.aau.dk>
2022-06-02 11:03:05 +0200

    add truncation study results

--- setup/run_all.m
+++ setup/run_all.m
@@ -1,12 +1,10 @@
 clear
-blabla = [7, 10, 15]/100;
 
-for abcd = 1:numel(blabla)
     damages = [1:14]';
     rng(1)
     damages(:, 2) = 0.80;
     
-    nsr = blabla(abcd);
+    nsr = 0.05;
     err = 0.00;
     sensor = "dis";
     in_dof = [1:12];
@@ -16,16 +14,15 @@ for abcd = 1:numel(blabla)
     blockrows = 48;
     n_runs = 50;
 
-    for truncated_mode = [0]
+    for truncated_mode = [12, 10]
         set_up
-        tic
+        t_start = tic;
         for foo = 1:size(damages, 1)
             damage = damages(foo, :);
             estimate_models
         end
-        toc
+        toc(t_start)
     end
-end
 
 beep
 pause(1)
