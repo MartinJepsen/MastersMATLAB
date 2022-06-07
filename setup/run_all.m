b771a36ddaf075b07223c09dbf7c2676e03b2bf5
MJ <mjeps17@student.aau.dk>
2022-06-07 09:08:40 +0200

    add results for sens3

--- setup/run_all.m
+++ setup/run_all.m
@@ -1,12 +1,12 @@
 clear
 
-    damages = [1:14]';
+    damages = [1]';
     rng(1)
     damages(:, 2) = 0.4;
     
     nsr = 0.05;
     sensor = "dis";
-    in_dof = [1,2,7,8];
+    in_dof = [1,2,7,8 ];
     out_dof = in_dof;
     dt = 0.0001;                            % time increment size
     n_samples = 15000;
@@ -19,11 +19,10 @@ clear
         t_start = tic;
         for foo = 1:size(damages, 1)
             damage = damages(foo, :);
-%             estimate_models
+            estimate_models
 %             gaindesign_1
-gaindesign_3            
-gaindesign_2
-            
+%             gaindesign_2
+%             gaindesign_3
         end
         toc(t_start)
     end
